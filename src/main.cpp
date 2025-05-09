8b07f7f23fb127e79244429e555a8b2b72e5859e
Bryan Legihono <yggcphanq@gamil.com>
2025-05-09 21:19:56 +0700

    Enhance audio control: add loop functionality and improve command handling

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int Player_state = 13;
 
 bool isPaused = false;
+bool isLoopEnabled = false;
 
 // Use pins 2 and 3 to communicate with DFPlayer Mini
 static const uint8_t PIN_MP3_TX = 27; // Connects to module's RX 
@@ -16,14 +17,16 @@ DFRobotDFPlayerMini player;
 
 void menu_opcoes() {
   Serial.println();
-  Serial.println("=======================================");
-  Serial.println("Commands:");
-  Serial.println("[1-3]    -> select the MP3 file");
-  Serial.println("[s]      -> stop the music");
-  Serial.println("[p]      -> pause/continue music");
-  Serial.println("[+ or -] -> increase or decrease audio volume");
+  Serial.println(F("======================================="));
+  Serial.println(F("Commands:"));
+  Serial.println(F("[1-3]    -> select the MP3 file"));
+  Serial.println(F("[s]      -> stop the music"));
+  Serial.println(F("[p]      -> pause/continue music"));
+  Serial.println(F("[+ or -] -> increase or decrease audio volume"));
+  Serial.println(F("[< or >] -> go to previous or next track"));
+  Serial.println(F("[l]      -> enable or disable loop"));
   Serial.println();
-  Serial.println("=======================================");
+  Serial.println(F("======================================="));
 }
 
 void setup() {
@@ -36,15 +39,15 @@ void setup() {
 
   Serial.println("Initializing DFPlayer Mini...");
 
-  if (!player.begin(softwareSerial)) {
+  if (player.begin(softwareSerial)) {
+    Serial.println("Initialization success!");
+  } else {
     Serial.println("Failed to initialize DFPlayer Mini!");
     Serial.println("Try this:");
     Serial.println("1. Check the DFPlayer Mini connections");
     Serial.println("2. Insert an SD card");
     return;
   }
-
-  Serial.println("Initialization success!");
 
   // Setups
   player.setTimeOut(500);
@@ -64,40 +67,74 @@ void loop() {
   if (Serial.available() > 0) {
     uint8_t command = Serial.read();
 
-    // Play music
-    if ((command >= '1') &&
-        (command <= '3')) {
-      Serial.printf("Music %c played.\n", command);
-      player.playMp3Folder(command - 48);
-    }
+    switch (command) {
+      // Play the music
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+        Serial.printf("Track %c played.\n", command);
+        player.playMp3Folder(command - 48);
+        break;
+      
+      // Stop the music
+      case 's':
+        player.stop();
+        Serial.println("Music has stopped.");
+        break;
+      
+      // Pause or continue the music
+      case 'p':
+        isPaused = !isPaused;
 
-    // Stop music
-    if (command == 's') {
-      player.stop();
-      Serial.println("Music has stopped.");
-    }
+        if (!isPaused) {
+          player.start();
+          Serial.println("Music continue...");
+        } else {
+          player.pause();
+          Serial.println("Music paused!");
+        }
+        break;
+      
+      // Increase the volume
+      case '+':
+        player.volumeUp();
+        Serial.printf("Current volume: %d", player.readVolume());
+        break;
+      
+      // Decrease the volume
+      case '-':
+        player.volumeDown();
+        Serial.printf("Current volume: %d", player.readVolume());
+        break;
+      
+      case '<':
+        player.previous();
+        Serial.printf("Track %d played.\n", player.readCurrentFileNumber());
+        break;
+      
+      case '>':
+        player.next();
+        Serial.printf("Track %d played.\n", player.readCurrentFileNumber());
+        break;
+      
+      case 'l':
+        isLoopEnabled = !isLoopEnabled;
 
-    // Pause/Continue music
-    if (command == 'p') {
-      isPaused = !isPaused;
-
-      if (!isPaused) {
-        player.start();
-        Serial.println("Music continue...");
-      } else {
-        player.pause();
-        Serial.println("Music paused!");
-      }
-    }
-
-    if (command == '+') {
-      player.volumeUp();
-      Serial.printf("Current volume: %d", player.readVolume());
-    }
-
-    if (command == '-') {
-      player.volumeDown();
-      Serial.printf("Current volume: %d", player.readVolume());
+        if (isLoopEnabled) {
+          player.enableLoop();
+          Serial.println("Track loop has been enabled");
+        } else {
+          player.disableLoop();
+          Serial.println("Track loop has been disabled");
+        }
+        
+        break;
+      
+      default:
+        Serial.println("Invalid command.");
+        break;
     }
 
     menu_opcoes();
